38785b84e001545fa6af9231f89ce882b440822c
xaviermichel <xavier.michel.mx440@gmail.com>
2013-12-04 21:37:53 +0100

    Adaptations

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,7 +1,7 @@
-alias ls="gls -F --color"
-alias l="gls -lAh --color"
-alias ll="gls -l --color"
-alias la='gls -A --color'
+alias ls="ls -F --color"
+alias l="ls -lAh --color"
+alias ll="ls -l --color"
+alias la='ls -A --color'
 alias s='cd ..'
 alias lh='ls -lh'
 
