4f3d1d47aaae74b3921ad434f1b34ebbb5a736bd
xaviermichel <xavier.michel.mx440@gmail.com>
2014-01-06 18:07:24 +0100

    Add aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,13 @@
 alias ls="ls -F --color"
 alias l="ls -lAh --color"
-alias ll="ls -l --color"
+alias ll="ls -lh --color"
 alias la='ls -A --color'
 alias s='cd ..'
 alias lh='ls -lh'
 
+# coloration de logs
+alias lesslog() { ccze -A < $1 | less -R; }
+alias logtail() { tail -f $1 | ccze -A; }
 
 # Run it as root ! (NOPASSWD)
 alias ssamba='sudo ~/bin/simple-samba.sh'
