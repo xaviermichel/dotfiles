23e4bc232d999f5b4476d746a603e4bcc1472b13
xaviermichel <xavier.michel.mx440@gmail.com>
2019-12-03 22:57:39 +0100

    feat: Update vim conf and aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -20,51 +20,14 @@ alias bmv='rsync -aP --remove-source-files'
 alias meteo='curl http://wttr.in/lyon'
 alias pluie="curl -s 'http://www.meteofrance.com/mf3-rpc-portlet/rest/pluie/692660' | jq -r '.niveauPluieText | join(\"\n\")'"
 
-# ------------------------------------
-# Docker alias and function from https://github.com/tcnksm/docker-alias
-# ------------------------------------
-
-# Get latest container ID
-alias dl="docker ps -l -q"
-
 # Get container process
 alias dps="docker ps"
 
 # Get process included stop container
 alias dpa="docker ps -a"
 
-# Get images
-alias di="docker images"
-
-# Get container IP
-alias dip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
-
-# Run deamonized container, e.g., $dkd base /bin/echo hello
-alias dkd="docker run -d -P"
-
-# Run interactive container, e.g., $dki base /bin/bash
-alias dki="docker run -i -t -P"
-
 # Execute interactive container, e.g., $dex base /bin/bash
 alias dex="docker exec -i -t"
-
-# Stop all containers
-dstop() { docker stop $(docker ps -a -q); }
-
-# Remove all containers
-drm() { docker rm $(docker ps -a -q); }
-
-# Stop and Remove all containers
-alias drmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
-
-# Remove all images
-dri() { docker rmi $(docker images -q); }
-
-# Dockerfile build, e.g., $dbu tcnksm/test
-dbu() { docker build -t=$1 .; }
-
-# Show all alias related docker
-dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
 
 # Bash into running container
 dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
